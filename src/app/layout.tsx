9d2d5ca3e92d74969aa21837cca854c35a17d53e
Jorge Ruanova <jruanova.dev@gmail.com>
2025-10-09 18:23:10 -0300

    testing meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Fast Track - Soluciones de gestion",
+  title: "Fast Track",
   description: "Soluciones de Gestion - SAS",
 };
 
